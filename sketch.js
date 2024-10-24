15f1119902e6a1095215f724424f8944d60777cb
Zac Morehouse <zacmorehouselogin@gmail.com>
2024-10-24 17:38:24 +1100

    Latest iteration with content

--- sketch.js
+++ sketch.js
@@ -1,29 +1,54 @@
-// Blink logic inspired by 
+// Blink Logic Refs :
 // Using the handsfreejs library https://handsfreejs.netlify.app/ 
 // https://storage.googleapis.com/tfjs-models/demos/facemesh/demo.fd0b9f10.js
 // https://age2death.glitch.me/
 // https://editor.p5js.org/golan/sketches/d-JfFcGws
-// alongside
 // https://editor.p5js.org/bestesaylar/sketches/ogFdasRIc
 // https://github.com/auduno/clmtrackr
 
+// Init Webcam Stuff
 let handsfreeTracker; 
 let webcamStream;
-let currentScene = 0;
 
+// Various vars for blink tracking
 let blinkHistory = [];
 const historyLength = 320; 
 let eyeOpennessAvg = 0; 
 let blinkIntensity = 0; 
-let blinkCount = 0; // Track the number of blinks
-let lastBlinkTime = 0; // Track time of last blink
+let blinkCount = 0;
+let lastBlinkTime = 0; 
 const blinkCooldown = 300; // Cooldown time in milliseconds between blinks
-const spacebarCooldown = 300; // Cooldown time for spacebar presses (1 second)
+const spacebarCooldown = 300; // Cooldown time for spacebar presses 
 
+// Additional Scene and Model Logic + Debugger
+let currentScene = 0;
 let debuggerMode = false; // Toggle for showing debug visuals
-let modelLoaded = false; // Track if the model is loaded
-let blinkDetected = false; // Track if a blink has occurred
-let lastSpacebarPress = 0; // Track time of last spacebar press
+let modelLoaded = false;
+let blinkDetected = false; 
+let lastSpacebarPress = 0; 
+
+// Text content and typing effect
+let textIndex = 0;
+let charIndex = 0; 
+let typingSpeed = 0; 
+let lastTypedTime = 0;
+let textY;  
+
+// Final text content and typing effect
+let finalText1 = "You blinked and the world rewrote itself.";
+let finalText2 = "Entangled in data streams. Are you still in control or just another line of code?";
+let finalCharIndex1 = 0;
+let finalCharIndex2 = 0;
+let finalTypingSpeed = 50; 
+let finalLastTypedTime = 0;
+
+// Restart Button Var
+let buttonX, buttonY, buttonWidth, buttonHeight;
+let isHovering = false;
+
+// Fading logic for Loading
+let fadeIn = true; 
+let fadeAlpha = 0; 
 
 /*
 let textLines = [
@@ -44,74 +69,47 @@ let textLines = [
   "A final blink... what do I see?"
 ];
 */
+
+// Aubade by Phillip Larkin
 let textLines = [
+  " ",
   "I work all day and get half-drunk at night",
   "Waking at four to soundless dark I stare",
   "In time the curtain-edges will grow light",
-  "Till then I see what’s really always there",
+  "Till then I see whats really always there",
+
   "Unresting death a whole day nearer now",
   "Making all thought impossible but how",
   "And where and when I shall myself die",
   "Arid interrogation yet the dread",
   "Of dying and being dead",
-  "Flashes afresh to hold and horrify",
-  "The mind blanks at the glare Not in remorse",
-  "The good not done the love not given time",
-  "Torn off unused nor wretchedly because",
-  "An only life can take so long to climb",
+
   "Clear of its wrong beginnings and may never",
   "But at the total emptiness for ever",
   "The sure extinction that we travel to",
   "And shall be lost in always Not to be here",
   "Not to be anywhere",
-  "And soon nothing more terrible nothing more true",
-  "This is a special way of being afraid",
-  "No trick dispels Religion used to try",
-  "That vast moth-eaten musical brocade",
-  "Created to pretend we never die",
+
   "And specious stuff that says No rational being",
   "Can fear a thing it will not feel not seeing",
   "That this is what we fear no sight no sound",
   "No touch or taste or smell nothing to think with",
   "Nothing to love or link with",
+
   "The anaesthetic from which none come round",
   "And so it stays just on the edge of vision",
   "A small unfocused blur a standing chill",
   "That slows each impulse down to indecision",
-  "Most things may never happen this one will",
-  "And realisation of it rages out",
-  "In furnace-fear when we are caught without",
-  "People or drink Courage is no good",
-  "It means not scaring others Being brave",
-  "Lets no one off the grave",
-  "Death is no different whined at than withstood",
-  "Slowly light strengthens and the room takes shape",
-  "It stands plain as a wardrobe what we know",
-  "Have always known know that we can’t escape",
-  "Yet can’t accept One side will have to go",
-  "Meanwhile telephones crouch getting ready to ring",
-  "In locked-up offices and all the uncaring",
-  "Intricate rented world begins to rouse",
-  "The sky is white as clay with no sun",
-  "Work has to be done",
-  "Postmen like doctors go from house to house"
+  "Most things may never happen.",
+  "This one will."
 ];
 
-
-let textY;  
-
-let textIndex = 0;
-let charIndex = 0; // Tracks the character being typed
-let typingSpeed = 50; // Speed of typing effect (in milliseconds)
-let lastTypedTime = 0; // Tracks the time of the last character typed
-
-let fadeIn = true; // Track whether we are in the fade-in or fade-out phase
-let fadeAlpha = 0; // Alpha value for fading
-
-
+// Preload
 function preload() {
   BusMatrixFont = loadFont('/assets/BusMatrixCondensed-Condensed.ttf');
 }
+
+// Setup Function
 function setup() {
   if (debuggerMode) {
     createCanvas(640, 480);
@@ -119,8 +117,9 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
   }
   
-  textFont(BusMatrixFont);
-  // Initialize blink history
+  textFont(BusMatrixFont); // Init New Font
+  
+  // Init Blink History
   blinkHistory = Array(historyLength).fill(0.1);
 
   // Set up webcam feed
@@ -135,10 +134,8 @@ function setup() {
     facemesh: true
   });
 
-  
-
   handsfreeTracker.start(() => {
-    modelLoaded = true; // Set to true when the model is fully loaded
+    modelLoaded = true; 
   });
 
   handsfreeTracker.hideDebugger();
@@ -147,30 +144,26 @@ function setup() {
 function draw() {
   background(0);
 
-  // If the model is still loading, display "Loading..."
+  // If the model is still loading, display loading text
   if (!modelLoaded) {
-    if (fadeIn) {
-      fadeAlpha += 5; // Increase alpha value
+    if (fadeIn) { // Fade in the loading
+      fadeAlpha += 5; 
       if (fadeAlpha >= 255) {
-        fadeIn = false; // Switch to fade-out once fully opaque
+        fadeIn = false; 
       }
     } else {
-      fadeAlpha -= 5; // Decrease alpha value
+      fadeAlpha -= 5; 
       if (fadeAlpha <= 0) {
-        fadeIn = true; // Switch to fade-in once fully transparent
+        fadeIn = true;
       }
     }
-
-    // Ensure alpha value stays within bounds
     fadeAlpha = constrain(fadeAlpha, 0, 255);
-
-    fill(255, fadeAlpha); // Apply the fade effect to the text
+    fill(255, fadeAlpha); 
     textAlign(CENTER, CENTER);
     textSize(40);
     text("Loading...", width / 2, height / 2);
-    return;  // Skip the rest of the draw loop until the model is loaded
+    return;
   }
-
 
   // Once the model is loaded, wait for a blink to continue
   if (!blinkDetected) {
@@ -179,11 +172,16 @@ function draw() {
     textSize(40);
     text("Blink to begin the artwork", width / 2, height / 2);
     textSize(20);
-
     text("Or Press Spacebar.", width / 2, height / 2 + 50);
-
     processBlinkDetection();
-    return; // Skip the rest of the draw loop until blink is detected
+    return;
+  }
+
+  // Once we get to the last line of text reset posterize filter and display final msg
+  if (blinkCount >= 26) {
+    posterizecount = 0;
+    displayFinalMessage();
+    return;
   }
 
   // Once a blink is detected, show the webcam in full screen
@@ -192,27 +190,31 @@ function draw() {
     processBlinkDetection();
 
     let faderLevel = map(blinkIntensity, 1, 0, 0, 255);
-    tint(faderLevel); // Apply tint to the webcam feed
+    tint(faderLevel); // Apply Tint
 
-    stroke(255, 15); // Light gray color for scan lines
-    for (let y = 0; y < height; y += 5) { // Line spacing
-      // Create a wave displacement based on the mouseX position
-      let bendAmount = map(mouseX, 0, width, -50, 50);  // Bend based on mouseX position (-50 to 50 range)
-      let offset = sin((y + frameCount) * 0.1) * bendAmount; // Sine wave controlled by frameCount and y position
-    
-      line(0 + offset, y, width + offset, y); // Apply the bend to the line
+    stroke(255, 15); // Apply Scan Lines
+    for (let y = 0; y < height; y += 5) { 
+      let offset = sin((y + frameCount) * 0.1);
+      line(0 + offset, y, width + offset, y); 
     }
+
+    // Posterize the feed as the sketch goes on
+    posterizecount = map(blinkCount, 0, 25, 15, 2);
+    posterizecount = constrain(posterizecount, 2, 15); 
+    filter(POSTERIZE, posterizecount);
 
     switch (currentScene) {
       case 0:
-        // No effect
-        break;
+        // Scene One
+      break;
       case 1:
-
-        break;
+        // Scene Two
+      break;
+      // Cont until 25
+          
     }
 
-    // Handle the typing animation
+    // Handle Typing Animation
     let currentText = textLines[textIndex];
 
     // Calculate the current time and compare with the last typed time
@@ -229,18 +231,18 @@ function draw() {
     textAlign(CENTER, CENTER);
     text(currentText.substring(0, charIndex), width / 2, textY);
 
+    // Write on Blink Count
     drawBlinkCountOnWebcamFrame();
   }
 
+  // Debugger Stuff
   if (debuggerMode) {
     drawFaceLandmarks();
     detectBlink();
   }
 }
 
-
 function drawBlinkCountOnWebcamFrame() {
-  // Define the padding value (same as the webcam frame's padding)
   let padding = 75;
 
   // Calculate aspect ratio of webcam stream
@@ -259,22 +261,18 @@ function drawBlinkCountOnWebcamFrame() {
   // Center the webcam image
   let x = (width - newWidth) / 2;
   let y = (height - newHeight) / 2;
-
-  // Set the blink count position relative to the bottom-left of the webcam frame
   fill(255);
-  textSize(32); // Match font size
+  textSize(32); 
   textAlign(LEFT, BOTTOM);
-  text(` ${blinkCount}`, x + 20, y + newHeight - 27); // Offset 20px from left and bottom of the webcam frame
+  text(` ${blinkCount}`, x + 20, y + newHeight - 26); 
 }
 
 
 function drawWebcamBackground() {
   push();
-  
-  // Define the padding value
   let padding = 75;
 
-  // Calculate aspect ratio of webcam stream
+  // Calculate aspect ratio of webcam stream again
   let webcamAspect = webcamStream.width / webcamStream.height;
   let canvasAspect = width / height;
 
@@ -282,7 +280,6 @@ function drawWebcamBackground() {
 
   // Check if the canvas is wider or taller than the webcam aspect ratio
   if (canvasAspect > webcamAspect) {
-    // Canvas is wider, fit by height and apply padding on width
     newHeight = height - 2 * padding;
     newWidth = newHeight * webcamAspect;
   } else {
@@ -297,7 +294,6 @@ function drawWebcamBackground() {
 
   textY = y + newHeight - 50;
 
-  
   // Draw the gradient tunnel effect
   drawGradientTunnel(x, y, newWidth, newHeight);
 
@@ -307,24 +303,18 @@ function drawWebcamBackground() {
   pop();
 }
 
-// Function to create a gradient around the webcam feed
+// Function to create a tunnel around the webcam feed
 function drawGradientTunnel(x, y, w, h) {
-  let gradientSteps = 20;  // Number of steps for the gradient
-
-  let innerColor = color(255, 255, 255, 150); // Lighter inner color (semi-transparent white)
-  let outerColor = color(0, 0, 0, 200); // Darker outer color (near-black)
+  let gradientSteps = 20;  
+  let innerColor = color(255, 255, 255, 150);
+  let outerColor = color(0, 0, 0, 200); 
 
   for (let i = 0; i < gradientSteps; i++) {
     let t = i / gradientSteps;
-
-    // Lerp between inner and outer colors
     let currentColor = lerpColor(innerColor, outerColor, t);
-
     stroke(currentColor);
     strokeWeight(1);
     noFill();
-
-    // Draw the rectangle slightly larger each time to create the gradient effect
     rect(x - i * 5, y - i * 5, w + i * 10, h + i * 10);
   }
 }
@@ -407,14 +397,15 @@ function updateBlinkStats() {
       blinkDetected = true;
 
       if (textIndex < textLines.length - 1) {
-        charIndex = 0; // Reset charIndex when moving to a new line
-        textIndex++;    // Only increment if not at the last line
+        charIndex = 0; 
+        textIndex++;    
         currentScene++;
       }
     }
   }
 }
 
+// Function to detect blink
 function detectBlink() {
   let historyScale = 500; 
 
@@ -447,6 +438,7 @@ function detectBlink() {
 function keyPressed() {
   let currentTime = millis();
 
+// If space pressed reset and move forward.
   if (key === ' ' && currentTime - lastSpacebarPress > spacebarCooldown) {
     blinkIntensity = 1.0;
     blinkCount++;
@@ -460,4 +452,86 @@ function keyPressed() {
     }
     print(`Blink #${blinkCount} incremented using spacebar at ${int(currentTime)} ms`);
   }
+
+// If r pressed restart the artwork
+  if (key === 'r') {
+    restartArtwork();
+  }
+}
+
+// Function to display final message after finish
+function displayFinalMessage() {
+  noStroke();
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(40);
+
+  // Typing effect for the first line
+  if (millis() - finalLastTypedTime > finalTypingSpeed) {
+    if (finalCharIndex1 < finalText1.length) {
+      finalCharIndex1++;
+    } else if (finalCharIndex2 < finalText2.length) {
+      finalCharIndex2++;
+    }
+    finalLastTypedTime = millis();
+  }
+
+  text(finalText1.substring(0, finalCharIndex1), width / 2, height / 2 - 50);
+  textSize(24);
+  text(finalText2.substring(0, finalCharIndex2), width / 2, height / 2);
+
+  buttonWidth = 150;
+  buttonHeight = 50;
+  buttonX = width / 2 - buttonWidth / 2;
+  buttonY = height / 2 + 100;
+
+  // Check if mouse is over the button
+  if (mouseX > buttonX && mouseX < buttonX + buttonWidth && mouseY > buttonY && mouseY < buttonY + buttonHeight) {
+    isHovering = true;
+  } else {
+    isHovering = false;
+  }
+
+  // Invert button
+  if (isHovering) {
+    fill(255); 
+    stroke(255); 
+  } else {
+    noFill();
+    stroke(255); 
+  }
+  
+  strokeWeight(1);
+  rect(buttonX, buttonY, buttonWidth, buttonHeight); 
+  noStroke();
+
+  if (isHovering) {
+    fill(0); 
+  } else {
+    fill(255); 
+  }
+  
+  textSize(24);
+  text("Restart", buttonX + buttonWidth / 2, buttonY + buttonHeight / 2 - 5);
+}
+
+function mousePressed() {
+  // Check if the mouse is over the button
+  if (mouseX > buttonX && mouseX < buttonX + buttonWidth && mouseY > buttonY && mouseY < buttonY + buttonHeight) {
+    restartArtwork(); // Call to restart the artwork
+  }
+}
+
+function restartArtwork() {
+  // Reset all necessary variables to restart the artwork
+  blinkCount = 0;
+  blinkDetected = false;
+  textIndex = 0;
+  charIndex = 0;
+  fadeAlpha = 0;
+  currentScene = 0;
+  finalCharIndex1 = 0;
+  finalCharIndex2 = 0;
+  finalTypingSpeed = 50; 
+  finalLastTypedTime = 0;
 }
